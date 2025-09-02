5c35bea405a1b69ba4988fb9d6f30eee9473a0c9
Ishan Deep <ishandeep48@gmail.com>
2025-09-02 19:31:49 +0530

    requirements

--- Model_API/main.py
+++ Model_API/main.py
@@ -85,4 +85,4 @@ async def predict_job(skillset : InputString):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app , host="0.0.0.0" , port=8000)
+    uvicorn.run(app , host="0.0.0.0" , port=7860)
